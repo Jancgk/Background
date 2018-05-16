127e1655d6f949ab1071c130e554aad07b8c8a32
beats0 <2047155291@qq.com>
2018-05-16 21:11:19 +0800

    fix Gal.min.js

--- js/GalMenu.js
+++ js/GalMenu.js
@@ -1,19 +1,19 @@
-(function($) {
+(function ($) {
     var GalMenu = {
         defaults: {
             click_to_close: true,
             stay_open: false
         },
-        init: function(options) {
+        init: function (options) {
             var o = options,
                 $this = $(this);
-            $this.each(function(i) {
+            $this.each(function (i) {
                 var $this = $(this),
                     settings = $.extend({},
                         GalMenu.defaults, o),
                     $menu = $('.' + settings.menu);
                 $this.on('mousedown',
-                    function(e) {
+                    function (e) {
                         if (e.which !== 3 && $(e.target).parents('.GalMenu').length < 1 && settings.click_to_close) {
                             $this.find('.GalMenu').stop(true, false).animate({
                                     opacity: 0
@@ -21,7 +21,7 @@
                                 {
                                     duration: 100,
                                     queue: false,
-                                    complete: function() {
+                                    complete: function () {
                                         $(this).css('display', 'none').find('.active').removeClass('active').next().stop(true, true).slideUp('normal')
                                     }
                                 });
@@ -30,7 +30,7 @@
                         }
                     });
                 $this.on('contextmenu',
-                    function(e) {
+                    function (e) {
                         e.preventDefault();
                         e.stopPropagation();
                         GalMenu.getCoords(e);
@@ -40,14 +40,14 @@
                             {
                                 duration: 100,
                                 queue: false,
-                                complete: function() {
+                                complete: function () {
                                     $(this).css('display', 'none')
                                 }
                             });
                         var audio = $("#audio")[0];
                         var add = 150;
                         var top = Coords.clientY - add,
-                            left = ($('body')[0] === e.target) ? Coords.clickX + add: Coords.clientX - add;
+                            left = ($('body')[0] === e.target) ? Coords.clickX + add : Coords.clientX - add;
                         $menu.css({
                             top: top + 'px',
                             left: left + 'px',
@@ -72,22 +72,25 @@
                     })
             })
         },
-        getCoords: function(e) {
-            var evt = e ? e: window.event;
+        getCoords: function (e) {
+            var evt = e ? e : window.event;
             var clickX = 0,
                 clickY = 0;
             if ((evt.clientX || evt.clientY) && document.body && document.body.scrollLeft != null) {
                 clickX = evt.clientX + document.body.scrollLeft;
                 clickY = evt.clientY + document.body.scrollTop
-            };
+            }
+            ;
             if ((evt.clientX || evt.clientY) && document.compatMode == 'CSS1Compat' && document.documentElement && document.documentElement.scrollLeft != null) {
                 clickX = evt.clientX + document.documentElement.scrollLeft;
                 clickY = evt.clientY + document.documentElement.scrollTop
-            };
+            }
+            ;
             if (evt.pageX || evt.pageY) {
                 clickX = evt.pageX;
                 clickY = evt.pageY
-            };
+            }
+            ;
             return Coords = {
                 clickX: clickX,
                 clickY: clickY,
@@ -98,7 +101,7 @@
             }
         }
     };
-    $.fn.GalMenu = function(method, options) {
+    $.fn.GalMenu = function (method, options) {
         if (GalMenu[method]) {
             return GalMenu[method].apply(this, Array.prototype.slice.call(arguments, 1))
         } else if (typeof method === 'object' || !method) {
@@ -108,14 +111,12 @@
         }
     }
 })(jQuery);
-String.prototype.removeWS = function() {
+String.prototype.removeWS = function () {
     return this.toString().replace(/\s/g, '')
 };
-String.prototype.pF = function() {
+String.prototype.pF = function () {
     return parseFloat(this)
 };
-Number.prototype.pF = function() {
+Number.prototype.pF = function () {
     return parseFloat(this)
-
-
 };
